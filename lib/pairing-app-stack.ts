cc133944771ff052f57a777d2bb8c2e84e7e535d
Christine-Anderson <114715340+Christine-Anderson@users.noreply.github.com>
2024-07-04 18:49:53 -0700

    Add API gateway

--- lib/pairing-app-stack.ts
+++ lib/pairing-app-stack.ts
@@ -1,5 +1,6 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as path from 'path';
 
@@ -8,20 +9,33 @@ export class PairingAppStack extends cdk.Stack {
         super(scope, id, props);
 
         // Define the Lambda function resource
-        const myFunction = new lambda.Function(this, "HelloWorldFunction", {
+        const myFunction = new lambda.Function(this, "myLambdaFunction", {
                 runtime: lambda.Runtime.PROVIDED_AL2023,
                 handler: "main",
                 code: lambda.Code.fromAsset(path.join(__dirname, '../lambda/function.zip')),
             });
 
-        // Define the Lambda function URL resource
-        const myFunctionUrl = myFunction.addFunctionUrl({
-            authType: lambda.FunctionUrlAuthType.NONE,
-        });
-
-        // Define a CloudFormation output for your URL
-        new cdk.CfnOutput(this, "myFunctionUrlOutput", {
-            value: myFunctionUrl.url,
+        const api = new apigateway.RestApi(this, "myApiGateway", {
+            defaultCorsPreflightOptions: {
+                allowHeaders: ["Content-type", "Authorization"],
+                allowMethods: ["GET", "POST"],
+                allowOrigins: ["*"]
+            },
         })
+
+        const integration = new apigateway.LambdaIntegration(myFunction)
+
+        const createGroupResource = api.root.addResource("create-group");
+        createGroupResource.addMethod("POST", integration);
+
+        const joinGroupResource = api.root.addResource("join-group");
+        joinGroupResource.addMethod("POST", integration);
+
+        const groupDetailsResource = api.root.addResource("group-details");
+        groupDetailsResource.addResource("{groupId}");
+        groupDetailsResource.addMethod("POST", integration);
+
+        const performMatchingResource = api.root.addResource("match");
+        performMatchingResource.addMethod("POST", integration);
     }
 }
